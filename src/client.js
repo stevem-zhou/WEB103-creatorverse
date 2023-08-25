e384cf1812771f4e2bb8876b9bbf2b270aa7ba51
Steven Zhou <stevenz9@uci.edu>
2023-08-24 22:18:27 -0700

    connected to database

--- src/client.js
+++ src/client.js
@@ -1,3 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const URL = 'insert your Project URL here';
+const URL = "https://uirnozfpjkiagalucbry.supabase.co";
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpcm5vemZwamtpYWdhbHVjYnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI5MzUxNTYsImV4cCI6MjAwODUxMTE1Nn0.rPH-FsIGhNzEQbZ8o7pm1qU0DGM03dK9-5l1YCWFGSo";
+export const supabase = createClient(URL, API_KEY);
